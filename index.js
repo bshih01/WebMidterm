3c034eea195b418aad903c9df2986a6c068d6326
Brandon Shih <branshih10@gmail.com>
2023-10-09 20:47:14 -0400

    refactoring navbar to have logo and integrated dropdown menues and subdropdown menues for the hamburger icon. added a header section.

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-const hamburgerDropdown = document.getElementById('hamburgerDropdown');
+let isOverHamburger = false;
+let isOverDropdown = false;
+let timeout;
 
-hamburgerMenu.addEventListener('click', () => {
-    if (hamburgerDropdown.style.display == 'none' ||
-        hamburgerDropdown.style.display == '') {
-            hamburgerDropdown.style.display = 'block';
-            hamburgerMenu.classList.add('active');
-    } else {
+function showDropdown() {
+    hamburgerDropdown.style.display = 'block';
+    hamburgerMenu.classList.add('active');
+}
+
+function hideDropdown() {
+    if (!isOverHamburger && !isOverDropdown) {
         hamburgerDropdown.style.display = 'none';
         hamburgerMenu.classList.remove('active');
+    }
+}
+
+hamburgerMenu.addEventListener('mouseenter', () => {
+    clearTimeout(timeout);
+    isOverHamburger = true;
+    showDropdown();
+});
+
+hamburgerMenu.addEventListener('mouseleave', () => {
+    isOverHamburger = false;
+    timeout = setTimeout(hideDropdown, 100);
+});
+
+hamburgerDropdown.addEventListener('mouseenter', () => {
+    clearTimeout(timeout);
+    isOverDropdown = true;
+});
+
+hamburgerDropdown.addEventListener('mouseleave', () => {
+    isOverDropdown = false;
+    timeout = setTimeout(hideDropdown, 100);
+});
+
+hamburgerMenu.addEventListener('click', () => {
+    if (hamburgerDropdown.style.display === 'none' ||
+        hamburgerDropdown.style.display === '') {
+        showDropdown();
+    } else {
+        hideDropdown();
     }
 });
 
 window.addEventListener('resize', () => {
-    if (window.innerWidth > 863) {
+    if (window.innerWidth > 752) {
         hamburgerDropdown.style.display = 'none';
         hamburgerMenu.classList.remove('active');
     }
