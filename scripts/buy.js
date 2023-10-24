3ee53ea49b017b3f5cc206e78f6ff139279c4b3b
Brandon Shih <branshih10@gmail.com>
2023-10-24 10:54:41 -0400

    adding krisha pfp and furniture images

--- scripts/buy.js
+++ scripts/buy.js
@@ -1,101 +1,101 @@
-var buy = [
+var furniture = [
         {
-            src: '../media/buy/b1.jpg',
+            src: '../media/furniture/b1.jpg',
             desc: 'Classic light grey couch',
             price: 90,
             contact: '1(302)-456-7390'
             
         },
         {
-            src: '../media/buy/b2.jpg',
+            src: '../media/furniture/b2.jpg',
             desc: 'Soft, very clean teal couch',
             price: 95,
             contact: '1(302)-125-6735'
             
         },
         {
-            src: '../media/buy/b3.jpg',
+            src: '../media/furniture/b3.jpg',
             desc: 'Modern olive green bean shaped couch',
             price: 110,
             contact: '@john_bowery'
        },
         {
-            src: '../media/buy/b4.jpg',
+            src: '../media/furniture/b4.jpg',
             desc: 'Delicate oak tree bed-side lamp',
             price: 25,
             contact: 'izzy.potter@gmail.com'
             
         },
         {
-            src: '../media/buy/b5.jpg',
+            src: '../media/furniture/b5.jpg',
             desc: 'Classic marble print bed-side lamp',
             price: 25,
             contact: '@este_123'
             
         },
         {
-            src: '../media/buy/b6.jpg',
+            src: '../media/furniture/b6.jpg',
             desc: 'Sun and moon bed-side lamp',
             price: 25,
             contact:'1(302)-472-8792'
         },
         {
-            src: '../media/buy/b7.jpg',
+            src: '../media/furniture/b7.jpg',
             desc: 'White, circular, soft, fury rug',
             price: 45,
             contact: '1(323)-863-2349'
             
         },
         {
-            src: '../media/buy/b8.jpg',
+            src: '../media/furniture/b8.jpg',
             desc: 'Fun egg rug',
             price: 30,
             contact: '1(781)-423-8763'
             
         },
         {
-            src: '../media/buy/b9.jpg',
+            src: '../media/furniture/b9.jpg',
             desc: 'Classic dark grey fury rug',
             price: 40,
             contact: 'jakedonnovan@gmail.com'
             
         },
         {
-            src: '../media/buy/b10.jpg',
+            src: '../media/furniture/b10.jpg',
             desc: 'Wavy stand floor lamp',
             price: 55,
             contact: 'jennygreen@gmail.com'
             
         },
         {
-            src: '../media/buy/b11.jpg',
+            src: '../media/furniture/b11.jpg',
             desc: 'Vintage floor lamp',
             price: 70,
             contact: '1(781)-465-8762'
             
         },
         {
-            src: '../media/buy/b12.jpg',
+            src: '../media/furniture/b12.jpg',
             desc: 'Hanging lamp shade floor lamp',
             price: 65,
             contact: '1(781)-421-6780'
             
         },
         {
-            src: '../media/buy/b13.jpg',
+            src: '../media/furniture/b13.jpg',
             desc: 'Modern beige bed-side table',
             price: 45,
-            contact: '@buymyfurnitureplease'
+            contact: '@furnituremyfurnitureplease'
         },
         {
-            src: '../media/buy/b14.jpg',
+            src: '../media/furniture/b14.jpg',
             desc: 'Minimalist white, light wood bed-side table',
             price: 40,
             contact: '@123letsgo'
             
         },
         {
-            src: '../media/buy/b15.jpg',
+            src: '../media/furniture/b15.jpg',
             desc: 'Dark wood geometric bed-side table',
             price: 45,
             contact: 'richard@gmail.com'    
@@ -129,7 +129,7 @@ var buy = [
             listingsContainer.appendChild(furniture);
         }
 
-        for (const item of buy) {
+        for (const item of furniture) {
             addAvailableFurniture(item.src, item.desc, item.price, item.contact);
         }
     });
